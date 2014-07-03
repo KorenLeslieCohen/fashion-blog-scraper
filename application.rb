7961999454089a6246619f09de6ef25cd98d4dfb
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-07-03 13:10:38 -0400

    last commit before initial deploy

--- application.rb
+++ application.rb
@@ -5,7 +5,7 @@ class Application < Sinatra::Base
 
   get '/' do
     @atlantic = FashionBlog.new("Atlantic-Pacific", '.post-body .separator', "http://atlantic-pacific.blogspot.com")
-    @sartorialist = FashionBlog.new("The Sartorialist", '.article-content p', "http://www.thesartorialist.com/")
+    # @sartorialist = FashionBlog.new("The Sartorialist", '.article-content p', "http://www.thesartorialist.com/")
     @sara_libby = FashionBlog.new("Sara Libby", '.post-body .separator', "http://www.saralibby.com/")
     # @courtney = FashionBlog.new("What Courtney Wore", '.entry-content p', "http://thecourtneykerr.com/")
     @we_wore = FashionBlog.new("We Wore What", '.entry-content p', "http://weworewhat.com/")
@@ -16,6 +16,13 @@ class Application < Sinatra::Base
     @pink_horror = FashionBlog.new("Pink Horrorshow", '.separator', "http://www.pinkhorrorshow.com/")
     # @trop = FashionBlog.new("Trop Rouge", '.post-body', "http://troprouge.blogspot.com/")
     # @jag = FashionBlog.new("Jag Lever", '.entry-content p', "http://www.jaglever.com/")
+    # @indie = FashionBlog.new("Fashion Indie", ".image a", "http://fashionindie.com/")
+    # @cupcakes = FashionBlog.new("Cupcakes and Cashmere", '.embedded-Media-image', "http://cupcakesandcashmere.com/")
+    # @toast = FashionBlog.new("Fashiontoast", '.copy p', "http://fashiontoast.com/")
+    # @marcy = FashionBlog.new("The Marcy Stop", '.separator', "http://www.themarcystop.com/")
+    @navy = FashionBlog.new("Something Navy", '.sn_pinterest', "http://www.somethingnavy.com/")
+    @viva = FashionBlog.new("Viva Luxury", '.separator a', "http://vivaluxury.blogspot.com/2012/10/mini-stripes.html")
+    @zoranna = FashionBlog.new("Zoranna", '.post-body a', "http://www.zorannah.com/")
     erb :"index.html"
   end
 
--- models/fashion.rb
+++ models/fashion.rb
@@ -7,7 +7,7 @@ require 'pry'
 
 class FashionBlog
 
-  attr_accessor :name, :post, :url, :image_links, :all_image_links, :jpg_links
+  attr_accessor :name, :post, :url, :image_links, :all_image_links, :jpg_links, :src_jpg_links
 
   ALL_BLOGS = []
 
@@ -18,6 +18,7 @@ class FashionBlog
     @image_links = []
     @all_image_links = []
     @jpg_links = []
+    @src_jpg_links = []
     ALL_BLOGS << self
   end
 
@@ -37,19 +38,19 @@ class FashionBlog
   end
 
   # working on method to scrape src
-  #   def get_links_img
-  #   doc = Nokogiri::HTML(open(url))
-  #   to_scrape = doc.css(post)
-  #   image_links = to_scrape.xpath('img')
-  #   all_image_links << image_links.map {|link| link["href"]}.compact
-  #   all_image_links.each do |inner_array|
-  #     inner_array.each do |link|
-  #       # binding.pry
-  #       jpg_links << link if link.include?(".jpg")
-  #     end
-  #   end
-  #   jpg_links
-  # end
+  def get_links_img
+    doc = Nokogiri::HTML(open(url))
+    to_scrape = doc.css(post)
+    image_links = to_scrape.xpath('img')
+    all_image_links << image_links.map {|link| link["src"]}.compact
+    all_image_links.each do |inner_array|
+      inner_array.each do |link|
+        # binding.pry
+        src_jpg_links << link 
+      end
+    end
+    src_jpg_links
+  end
 
   def self.all_blogs
     ALL_BLOGS
